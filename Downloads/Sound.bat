8c395116ce74507df5adaba3cb7cd54409eb20fe
Trevin <132922589+T-Crypt@users.noreply.github.com>
2025-01-11 08:52:12 -0700

    Update Sound.bat

--- Downloads/Sound.bat
+++ Downloads/Sound.bat
@@ -1,34 +1,48 @@
 @echo off
-
-:: Realtek Audio Bloat.
+:: Define Paths
 set "DriverStorePath=C:\Windows\System32\DriverStore\FileRepository"
 set "FilePattern=realtekservice.inf_amd64_*"
+set "System32Path=C:\Windows\System32"
+set "LogFile=cleanup.log"
 
-:: Task Killing, Stopping and Disabling Realtek.
+:: Initialize Log
+echo Cleanup Script Started - %date% %time% > "%LogFile%"
+
+:: Realtek Audio Cleanup
+echo Cleaning Realtek Audio... >> "%LogFile%"
 taskkill /F /IM RtkAudUService64.exe >nul 2>&1
-net stop "RtkAudioUniversalService" >nul 2>&1
-sc config RtkAudioUniversalService start=disabled >nul 2>&1
-
-:: Use a for loop to find all matching folders/files
+sc query RtkAudioUniversalService >nul 2>&1 && (
+    net stop "RtkAudioUniversalService" >nul 2>&1
+    sc config RtkAudioUniversalService start=disabled >> "%LogFile%" 2>&1
+)
 for /d %%G in ("%DriverStorePath%\%FilePattern%") do (
-    echo Deleting: %%G >nul 2>&1
+    echo Deleting: %%G >> "%LogFile%"
     rd /s /q "%%G" >nul 2>&1
 )
 
-:: Sound Research Audio Bloat.
+:: Sound Research Audio Cleanup
+echo Cleaning Sound Research Audio... >> "%LogFile%"
 taskkill /f /im SECOMNService.exe >nul 2>&1
 taskkill /f /im SECOCL.exe >nul 2>&1
-net stop "SECOMNService" >nul 2>&1
-sc config SECOMNService start=disabled >nul 2>&1
-rd /s /q "C:\Windows\System32\SECOMN64.exe" >nul 2>&1
-rd /s /q "C:\Windows\System32\SECOCL64.exe" >nul 2>&1
-rd /s /q "C:\Windows\System32\SECCNH64.exe" >nul 2>&1
+sc query SECOMNService >nul 2>&1 && (
+    net stop "SECOMNService" >nul 2>&1
+    sc config SECOMNService start=disabled >> "%LogFile%" 2>&1
+)
+del /f /q "%System32Path%\SECOMN64.exe" >nul 2>&1
+del /f /q "%System32Path%\SECOCL64.exe" >nul 2>&1
+del /f /q "%System32Path%\SECCNH64.exe" >nul 2>&1
 
-:: VisiSonics Audio Bloat.
+:: VisiSonics Audio Cleanup
+echo Cleaning VisiSonics Audio... >> "%LogFile%"
 taskkill /f /im VSHelper.exe >nul 2>&1
 taskkill /f /im VSSrv.exe >nul 2>&1
-net stop "VSSrv" >nul 2>&1
-sc config VSSrv start=disabled >nul 2>&1
-rd /s /q "C:\Windows\System32\VSHelper.exe" >nul 2>&1
-rd /s /q "C:\Windows\System32\VSSrv.exe" >nul 2>&1
+sc query VSSrv >nul 2>&1 && (
+    net stop "VSSrv" >nul 2>&1
+    sc config VSSrv start=disabled >> "%LogFile%" 2>&1
+)
+del /f /q "%System32Path%\VSHelper.exe" >nul 2>&1
+del /f /q "%System32Path%\VSSrv.exe" >nul 2>&1
+
+:: Finalize
+echo Cleanup Completed - %date% %time% >> "%LogFile%"
 exit
